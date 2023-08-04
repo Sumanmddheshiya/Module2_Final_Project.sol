47ead76342a2b7c93f56e434028202127197f6f4
Sumanmddheshiya <129781628+Sumanmddheshiya@users.noreply.github.com>
2023-08-04 21:32:30 +0530

    Update smartContract.sol

--- MODULE-2_FINAL PROJECT/smartContract.sol
+++ MODULE-2_FINAL PROJECT/smartContract.sol
@@ -1,15 +1,30 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-contract MyContract {
-  uint public cryptocurrency;
+contract SimpleContract {
+    string[] private suman;
 
-  function incrementCryptocurrency() public {
-    cryptocurrency += 1;
-  }
+    function setName(string memory _name) public {
+        suman.push(_name);
+    }
 
-  function getCryptocurrency() public view returns (uint) {
-    return cryptocurrency;
-  }
+    function getAllNames() public view returns (string[] memory) {
+        return suman;
+    }
+
+    function reset() public {
+        delete suman;
+    }
+
+    function deleteName(string memory _gupta) public {
+        for (uint256 i = 0; i < suman.length; i++) {
+            if (keccak256(bytes(suman[i])) == keccak256(bytes(_gupta))) {
+                for (uint256 j = i; j < suman.length - 1; j++) {
+                    suman[j] = suman[j + 1];
+                }
+                suman.pop();
+                break;
+            }
+        }
+    }
 }
