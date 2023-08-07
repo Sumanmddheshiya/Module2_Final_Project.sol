e3167f61b25d2f257fe286810d728d93d716ff6c
Sumanmddheshiya <129781628+Sumanmddheshiya@users.noreply.github.com>
2023-08-07 10:50:07 +0530

    Update smartContract.sol

--- MODULE-2_FINAL PROJECT/smartContract.sol
+++ MODULE-2_FINAL PROJECT/smartContract.sol
@@ -1,30 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+// SimpleContract.sol
+pragma solidity ^0.8.0;
 
 contract SimpleContract {
-    string[] private suman;
+    uint256 public value;
 
-    function setName(string memory _name) public {
-        suman.push(_name);
+    function setValue(uint256 _newValue) public {
+        value = _newValue;
     }
 
-    function getAllNames() public view returns (string[] memory) {
-        return suman;
-    }
-
-    function reset() public {
-        delete suman;
-    }
-
-    function deleteName(string memory _gupta) public {
-        for (uint256 i = 0; i < suman.length; i++) {
-            if (keccak256(bytes(suman[i])) == keccak256(bytes(_gupta))) {
-                for (uint256 j = i; j < suman.length - 1; j++) {
-                    suman[j] = suman[j + 1];
-                }
-                suman.pop();
-                break;
-            }
-        }
+    function getValue() public view returns (uint256) {
+        return value;
     }
 }
